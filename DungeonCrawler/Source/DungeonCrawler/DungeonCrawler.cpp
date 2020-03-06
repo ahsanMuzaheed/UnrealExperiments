c4968cb9f95c8dd06169702898b9d266a09d4a51
Alekssasho <aleksandar.angelovv@gmail.com>
2020-03-06 08:55:03 +0200

    Added top down template

---
+++ DungeonCrawler/Source/DungeonCrawler/DungeonCrawler.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "DungeonCrawler.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonCrawler, "DungeonCrawler" );
+
+DEFINE_LOG_CATEGORY(LogDungeonCrawler)
+ 
